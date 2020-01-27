f50e22a19794fceba9050074ce79da7915700f5c
juliodeblas <julio.deblas@cesjuanpablosegundo.es>
2020-01-27 10:04:13 +0100

    Fragments Personas

---
+++ app/src/main/java/com/example/proyectopersonas/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.proyectopersonas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.proyectopersonas.fragments.FragmentPersonas;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.sitio_fragments, new FragmentPersonas(), "personas");
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+}
---
+++ app/src/main/java/com/example/proyectopersonas/fragments/FragmentCabecera.java
@@ -0,0 +1,41 @@
+package com.example.proyectopersonas.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.proyectopersonas.R;
+
+public class FragmentCabecera extends Fragment {
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.layout_fragment_cabecera, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+}
